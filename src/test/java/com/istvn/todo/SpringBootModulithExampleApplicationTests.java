2ac6dbea7e1d3d7afe74d2d75c5a3adb31b92995
ISTVN <istvan.kuli1@gmail.com>
2024-09-19 14:49:56 +0200

    added controllers and response classes

--- src/test/java/com/istvn/todo/SpringBootModulithExampleApplicationTests.java
+++ src/test/java/com/istvn/todo/SpringBootModulithExampleApplicationTests.java
@@ -2,6 +2,8 @@ package com.istvn.todo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 @SpringBootTest
 class SpringBootModulithExampleApplicationTests {
@@ -9,5 +11,14 @@ class SpringBootModulithExampleApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	@Test
+    void writeDocumentationSnippets() {
+        var modules = ApplicationModules.of(SpringBootModulithExampleApplication.class).verify();
+        new Documenter(modules)
+            .writeModulesAsPlantUml()
+            .writeIndividualModulesAsPlantUml()
+            .writeDocumentation();
+    }
 
 }
